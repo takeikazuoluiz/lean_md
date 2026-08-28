da08ccdfd31822a2e284573b53936bee7e7beb36
takeikazuoluiz <takeikazuoluiz@gmail.com>
2026-08-28 06:18:57 -0400

    t

--- talk/Talk/Basic.lean
+++ talk/Talk/Basic.lean
@@ -1,3 +1,7 @@
+import Mathlib.Data.Real.Basic
+
+--set_option linter.style.header false
+
 def hello := "world"
 
 example (p q : Prop) : p ∧ q → p ∨ q := by
--- talk/Talk/analysis.lean
+++ talk/Talk/analysis.lean
@@ -1,2 +1,7 @@
-example (p q : Prop) : p ∧ q → p ∨ q := by
-  sorry
+import Mathlib.Data.Real.Basic
+
+def converges_to (f : ℝ → ℝ) (a : ℝ) (l : ℝ) :=
+  ∀ ε > 0, ∃ δ > 0, ∀ (x : ℝ), |x - a| < δ → |f x - l| < ε
+
+theorem lim_sum (f g : ℝ → ℝ)
+  (hf : converges_to f a l1) (hg : converges_to g a l2) :
